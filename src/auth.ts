ee9b4b294e41072692b346d1cb431637a015b50a
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-03-17 05:59:16 +0900

    style: format by biome

--- src/auth.ts
+++ src/auth.ts
@@ -1,26 +1,31 @@
-import { getLoginUrl, getSessionToken } from "./login.js"
-import { input, redirectLinkParser } from "./util.js"
+import { getLoginUrl, getSessionToken } from "./login.js";
+import { input, redirectLinkParser } from "./util.js";
 import type { Option } from "@sniptt/monads";
 import { Some, None } from "@sniptt/monads";
 
 export async function auth(): Promise<string> {
-    try {
-        const auth_params = getLoginUrl();
+	try {
+		const auth_params = getLoginUrl();
 
-        console.log("Auth URL: " + "\u001b[32m" + auth_params.url + "\u001b[0m"); // green text
+		console.log("Auth URL: " + "\u001b[32m" + auth_params.url + "\u001b[0m"); // green text
 
-        const login_url = redirectLinkParser(await input("Please jump to the following URL, copy the URL starting with 'np5c38e31cd085304b://' and paste it into the standard input: "));
+		const login_url = redirectLinkParser(
+			await input(
+				"Please jump to the following URL, copy the URL starting with 'np5c38e31cd085304b://' and paste it into the standard input: ",
+			),
+		);
 
-        const session_token_code: string = login_url.session_token_code;
-        const code_verifier = auth_params.code_verifier;
-        const session_token = (await getSessionToken(session_token_code, code_verifier)).unwrap();
+		const session_token_code: string = login_url.session_token_code;
+		const code_verifier = auth_params.code_verifier;
+		const session_token = (
+			await getSessionToken(session_token_code, code_verifier)
+		).unwrap();
 
-        console.log("session_token: " + "\u001b[32m" + session_token + "\u001b[0m");
+		console.log("session_token: " + "\u001b[32m" + session_token + "\u001b[0m");
 
-        return session_token;
-    }
-    catch (error) {
-        console.error(error);
-        return "";
-    }
+		return session_token;
+	} catch (error) {
+		console.error(error);
+		return "";
+	}
 }
--- src/exec.ts
+++ src/exec.ts
@@ -1,12 +1,15 @@
-import { getServiceToken } from "./login.js"
-import { getGameList, parseGameList } from "./game.js"
+import { getServiceToken } from "./login.js";
+import { getGameList, parseGameList } from "./game.js";
 import * as dotenv from "dotenv";
 
-export async function exec(): Promise<{ title: string, icon: string, total_played_hours: number }[]> {
-  dotenv.config();
-  const session_token: string = process.env.SESSION_TOKEN!;
+export async function exec(): Promise<
+	{ title: string; icon: string; total_played_hours: number }[]
+> {
+	dotenv.config();
+	const session_token: string = process.env.SESSION_TOKEN!;
 
-  const service_id_token = (await getServiceToken(session_token)).unwrap().id_token;
+	const service_id_token = (await getServiceToken(session_token)).unwrap()
+		.id_token;
 
-  return parseGameList((await getGameList(service_id_token)).unwrap());
+	return parseGameList((await getGameList(service_id_token)).unwrap());
 }
